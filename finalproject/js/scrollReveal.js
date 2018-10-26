1634f93e329a2c23a77832958d83b64c43b70887
dannyboy96s <dflorescu96@hotmail.com>
2018-10-25 17:04:21 -0700

    added project files

---
+++ finalproject/js/scrollReveal.js
@@ -0,0 +1,167 @@
+window.sr = ScrollReveal();
+
+// sr.reveal('.heading',{
+
+//     duration: 1750,
+
+//     origin: 'bottom'
+
+// });
+
+sr.reveal('.scroll-left',{
+
+    duration: 1750,
+
+    origin: 'left',
+
+    distance: '300px'
+
+});
+
+sr.reveal('.scroll-bottom',{
+
+    duration: 1750,
+
+    origin: 'bottom',
+
+    delay: 750,
+
+    distance: '300px'
+
+});
+
+sr.reveal('.scroll-right',{
+
+    duration: 1750,
+
+    origin: 'right',
+
+    distance: '300px'
+
+});
+
+sr.reveal('.showcase-btn',{
+
+    duration: 1750,
+
+    delay: 1500,
+
+    origin: 'bottom',
+
+
+
+});
+
+sr.reveal('.p1',{
+
+    duration: 1750,
+
+    origin: 'top',
+
+    distance: '300px'
+
+});
+
+sr.reveal('.p2',{
+
+    duration: 1750,
+
+    delay: 1500,
+
+    origin: 'right',
+
+    distance: '300px'
+
+});
+
+sr.reveal('.p3',{
+
+    duration: 1750,
+
+    delay: 1500,
+
+    origin: 'left',
+
+    distance: '300px'
+
+});
+
+sr.reveal('.p4',{
+
+    duration: 1750,
+
+    origin: 'bottom',
+
+    distance: '300px'
+
+});
+
+sr.reveal('.contact-icons',{
+
+    duration: 1750,
+
+    origin: 'left',
+
+    distance: '300px',
+
+    viewFactor: 0.2
+
+});
+
+// sr.reveal('#testimonal div',{
+
+//     duration: 1750,
+
+//     origin: 'bottom'
+
+
+
+// });
+
+// sr.reveal('.specs-left',{
+
+//     duration: 1750,
+
+//     origin: 'left',
+
+//     distance: '300px',
+
+//     viewFactor: 0.2
+
+// });
+
+// sr.reveal('.specs-right',{
+
+//     duration: 1750,
+
+//     origin: 'right',
+
+//     distance: '300px',
+
+//     viewFactor: 0.2
+
+// });
+
+// sr.reveal('.buy-left',{
+
+//     duration: 1750,
+
+//     origin: 'left',
+
+//     distance: '300px',
+
+//     viewFactor: 0.2
+
+// });
+
+// sr.reveal('.buy-right',{
+
+//     duration: 1750,
+
+//     origin: 'bottom',
+
+//     distance: '300px',
+
+//     viewFactor: 0.2
+
+// });
